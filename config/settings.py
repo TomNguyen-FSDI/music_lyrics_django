0ae6d8cea1a188b6e1b63c3a762c7aac99712779
Tom <tommie_89@yahoo.com>
2021-05-09 18:56:00 -0700

    started over

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'music_lyrics.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'music_lyrics.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
